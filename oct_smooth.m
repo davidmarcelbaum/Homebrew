7cf56a51350ce33a8181d20b8f35cdb93d07692f
davidmarcelbaum <david.baum@tuta.io>
2020-12-23 10:25:27 -0300

    Attempt to increase speed

--- oct_smooth.m
+++ oct_smooth.m
@@ -15,6 +15,7 @@ for i_init = 1:s_smoothing/2
     % Increasing number of data points available
     s_span = i_init - 1 + i_init;
     v_smoothed(i_init) = sum(v_in(1:s_span)) / s_span;
+    % v_smoothed(i_init) = mean(v_in(1:s_span));
 end
 
 % Attempt to speed up this for loop since it's taking the longest and has 
@@ -23,6 +24,7 @@ idx_span = zeros(1, s_smoothing);
 for i_full = i_init+1:numel(v_in)-s_smoothing/2
     idx_span           = i_full+1-s_smoothing/2:i_full+s_smoothing/2;
     v_smoothed(i_full) = sum(v_in(idx_span)) / s_smoothing;
+    % v_smoothed(i_full) = mean(v_in(idx_span));
 end
 for i_end = i_full+1:numel(v_in)-1
     % Decreasing number of data points available
@@ -30,6 +32,7 @@ for i_end = i_full+1:numel(v_in)-1
     idx_v    = i_end-diff2end:i_end+diff2end;
     s_span   = numel(idx_v);
     v_smoothed(i_end) = sum(v_in(idx_v)) / s_span;
+    % v_smoothed(i_end) = mean(v_in(idx_v));
 end
 v_smoothed(size_in) = v_in(end);
 
